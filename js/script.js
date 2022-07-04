b440ed416cf645e6bbe12f8aa5226bd5e77ccb48
AirlineDog <t8190042@aueb.gr>
2022-07-04 13:53:02 +0100

    Add history command

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const consoleInput = document.querySelector(".console-input");
 const historyContainer = document.querySelector(".console-history");
 const consoleOutput = document.querySelector(".input-line");
+const history = [];
 
 function addResult(inputString, output){
     const outputString = output.toString();
@@ -22,10 +23,15 @@ consoleInput.addEventListener("keyup", e => {
         return;
     }
 
-    if (e.key ==="Enter"){
+    if (e.key === "Enter"){
+        history.push(command);
+        
         switch (command.toLowerCase()) {
             case "help":
                 printLines(command, help);
+                break;
+            case "history":
+                printHistory(command);
                 break;
             default:
                 addResult(command, `${command}: Command not found. Enter 'help' for list of available commands.`) 
@@ -57,10 +63,32 @@ function printLines(input, command){
     outputLogElement.classList.add("console-output-log");
     
     inputLogElement.textContent = `airlinedog@aueb.gr:~$ ${input}`;
+
     var text = "";
     for (var i = 0; i < command.length; i++) {
         text += command[i].replace("  ", "&nbsp;".repeat(20+ 23-command[i].trim().split(/\s+/)[1].length));
     }
     outputLogElement.innerHTML = text;
+
     historyContainer.append(inputLogElement, outputLogElement);
+}
+
+function printHistory(input){
+    const inputLogElement = document.createElement("div");
+    const outputLogElement = document.createElement("div");
+
+    inputLogElement.classList.add("console-input-log");
+    outputLogElement.classList.add("console-output-log");
+    inputLogElement.textContent = `airlinedog@aueb.gr:~$ ${input}`;
+
+    var text = "<br>";
+    for (var i = 0; i < history.length; i++) {
+        text += `<span class="history">${history[i]}</span><br>`;
+    }
+    text += "<br>";
+    outputLogElement.innerHTML = text;
+
+
+    historyContainer.append(inputLogElement, outputLogElement);
+
 }
