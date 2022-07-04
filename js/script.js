ada6c9eabbe45d11e1c4356549bf6c3ee44ee641
AirlineDog <t8190042@aueb.gr>
2022-07-04 10:46:40 +0100

    Add cursor functionality

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const consoleInput = document.querySelector(".console-input");
 const historyContainer = document.querySelector(".console-history");
+const consoleOutput = document.querySelector(".input-line");
 
 function addResult(inputString, output){
     const outputString = output instanceof Array ? `[${output.join(", ")}]` : output.toString();
@@ -30,9 +31,16 @@ consoleInput.addEventListener("keyup", e => {
         
         consoleInput.value = "";
         historyContainer.scrollTop = historyContainer.scrollHeight;
+        consoleOutput.innerHTML = '<span id="typer"></span><b class="cursor" id="cursor">█</b>';
     }
 });
 
 function focusOnInput(){
     document.querySelector(".console-input").focus();
 }
+
+const inputHandler = function(e) {
+    consoleOutput.innerHTML = e.target.value + '<b class="cursor" id="cursor">█</b>';
+}
+
+consoleInput.addEventListener("input", inputHandler);
