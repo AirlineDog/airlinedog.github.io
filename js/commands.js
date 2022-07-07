7c329d8669f17427be34de34c26d2b1b096a5527
AirlineDog <t8190042@aueb.gr>
2022-07-07 10:29:56 +0100

    Reshape social

--- js/commands.js
+++ js/commands.js
@@ -2,11 +2,11 @@ help = [
     '<br><span class="command">whois</span>          <span class="command-help">Who is Alex?</span><br>',
     '<span class="command">whoami</span>         <span class="command-help">Who are you?</span><br>',
     '<span class="command">social</span>         <span class="command-help">Display social networks</span><br>',
-    '<span class="command">secret</span>         <span class="command-help">Find the password</span><br>',
-    '<span class="command">projects</span>       <span class="command-help">View coding projects</span><br>',
+    //'<span class="command">secret</span>         <span class="command-help">Find the password</span><br>',
+    //'<span class="command">projects</span>       <span class="command-help">View coding projects</span><br>',
     '<span class="command">history</span>        <span class="command-help">View command history</span><br>',
     '<span class="command">help</span>           <span class="command-help">You obviously already know what this does</span><br>',
-    '<span class="command">email</span>          <span class="command-help">Feel free to email me</span><br>',
+    //'<span class="command">email</span>          <span class="command-help">Feel free to email me</span><br>',
     '<span class="command">clear</span>          <span class="command-help">Clear terminal</span><br><br>',
 ];
 
@@ -18,8 +18,8 @@ whois = [
 ];
 
 social = [
-    "<br>Find me in:<br><br>",
-    '<a href="https://github.com/AirlineDog">Github</a><br>',
-
-    '<a href="https://www.linkedin.com/in/alexios-doganis-a962041b9">LinkedIn</a><br><br>'
+    "<br>Find me on:<br><br>",
+    '<a href="https://github.com/AirlineDog" target="_blank"><img src="images/github-logo.png" class="social"></a>',
+    '<a href="https://www.linkedin.com/in/alexios-doganis-a962041b9" target="_blank"><img src="images/linkedin.png" class="social"></a>',
+    '<a href="https://www.instagram.com/airlinedog" target="_blank"><img src="images/instagram.png" class="social"></a><br>',
 ];
