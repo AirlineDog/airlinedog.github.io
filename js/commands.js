84dac80cda697dc45d0f2b121cc384f8a58cb8bd
AirlineDog <t8190042@aueb.gr>
2022-07-08 11:37:09 +0100

    Add banner

--- js/commands.js
+++ js/commands.js
@@ -1,25 +1,41 @@
 help = [
-    '<br><span class="command">whois</span>          <span class="command-help">Who is Alex?</span><br>',
-    '<span class="command">whoami</span>         <span class="command-help">Who are you?</span><br>',
-    '<span class="command">social</span>         <span class="command-help">Display social networks</span><br>',
-    //'<span class="command">secret</span>         <span class="command-help">Find the password</span><br>',
-    //'<span class="command">projects</span>       <span class="command-help">View coding projects</span><br>',
-    '<span class="command">history</span>        <span class="command-help">View command history</span><br>',
-    '<span class="command">help</span>           <span class="command-help">You obviously already know what this does</span><br>',
-    //'<span class="command">email</span>          <span class="command-help">Feel free to email me</span><br>',
-    '<span class="command">clear</span>          <span class="command-help">Clear terminal</span><br><br>',
+    '<br><span class="command">whois</span>          <span class="command-help">Who is Alex?</span>',
+    '<span class="command">whoami</span>         <span class="command-help">Who are you?</span>',
+    '<span class="command">social</span>         <span class="command-help">Display social networks</span>',
+    //'<span class="command">secret</span>         <span class="command-help">Find the password</span>',
+    //'<span class="command">projects</span>       <span class="command-help">View coding projects</span>',
+    '<span class="command">history</span>        <span class="command-help">View command history</span>',
+    '<span class="command">help</span>           <span class="command-help">You obviously already know what this does</span>',
+    //'<span class="command">email</span>          <span class="command-help">Feel free to email me</span>',
+    '<span class="command">clear</span>          <span class="command-help">Clear terminal</span><br>',
 ];
 
 whois = [
-    "<br>Hey, I'm Alex Doganis<br><br>",
-    "I am currently studying at the Department of Management Science & Technology<br>",
-    " of Athens University of Economics and Business, majoring in Software and Data Analysis Technologies.<br>",
-    "I am looking froward for opportunities to work in the field of software development.<br>",
+    "<br>Hey, I'm Alex Doganis<br>",
+    "I am currently studying at the Department of Management Science & Technology",
+    " of Athens University of Economics and Business, majoring in Software and Data Analysis Technologies.",
+    "I am looking froward for opportunities to work in the field of software development.",
 ];
 
 social = [
-    "<br>Find me on:<br><br>",
-    '<a href="https://github.com/AirlineDog" target="_blank"><img src="images/github-logo.png" class="social"></a>',
-    '<a href="https://www.linkedin.com/in/alexios-doganis-a962041b9" target="_blank"><img src="images/linkedin.png" class="social"></a>',
-    '<a href="https://www.instagram.com/airlinedog" target="_blank"><img src="images/instagram.png" class="social"></a><br>',
+    "<br>Find me on:",
+    `<a href="https://github.com/AirlineDog" target="_blank"><img src="images/github-logo.png" class="social"></a>
+     <a href="https://www.linkedin.com/in/alexios-doganis-a962041b9" target="_blank"><img src="images/linkedin.png" class="social"></a>
+     <a href="https://www.instagram.com/airlinedog" target="_blank"><img src="images/instagram.png" class="social"></a>`
 ];
+
+banner = [
+"       __ _.--..--._ _",
+"    .-' _/   _/\\_   \\_'-.",
+"   |__ /   _/\\__/\\_   \\__|",
+"      |___/\\_\\__/  \\___|               ____  _        ___  __ __      ___     ___    ____   ____  ____   ____ _____",
+"             \\__/                     /    || |      /  _]|  |  |    |   \\   /   \\  /    | /    ||    \\ |    / ___/",
+"             \\__/                    |  o  || |     /  [_ |  |  |    |    \\ |     ||   __||  o  ||  _  | |  (   \\_ ",
+"              \\__/                   |     || |___ |    _]|_   _|    |  D  ||  O  ||  |  ||     ||  |  | |  |\\__  |",
+"               \\__/                  |  _  ||     ||   [_ |     |    |     ||     ||  |_ ||  _  ||  |  | |  |/  \\ |",
+"            ____\\__/___              |  |  ||     ||     ||  |  |    |     ||     ||     ||  |  ||  |  | |  |\\    |",
+"      . - '             ' -.         |__|__||_____||_____||__|__|    |_____| \\___/ |___,_||__|__||__|__||____|\\___|",
+"     /                      \\                                                                                © 2022",
+"~~~~~~~  ~~~~~ ~~~~~  ~~~ ~~~  ~~~~~",
+" ~~~   ~~~~~   ~!~~   ~~ ~  ~ ~ ~~~ ",
+]
